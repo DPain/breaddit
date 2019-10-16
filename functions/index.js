1a2c66b3939b656977ef828e5a66485a30776343
DPain <dpainhahn@gmail.com>
2019-10-16 17:43:28 -0400

    Added example Firebase Functions that uses Express.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.use(cors({ origin: true }));
+
+
+app.get('/users/:id', (req, res) => res.send(`GET /api/users/${req.params.id} here`));
+app.post('/users', (req, res) => res.send(`POST here`));
+app.put('/users/:id', (req, res) => res.send(`PUT here`));
+app.delete('/users/:id', (req, res) => res.send(`DELETE here`));
+app.get('/users', (req, res) => res.send(`GET users here`));
+
+// Expose Express API as a single Cloud Function:
+exports.api = functions.https.onRequest(app);
