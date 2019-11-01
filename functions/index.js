1961b66cb8f42ebcbd5c1d3c3f6b5a2e5636796f
DPain <dpainhahn@gmail.com>
2019-11-01 03:39:05 -0400

    Fixed some CORS issues.

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ const verifyToken = require('./auth');
 
 const app = express();
 
-var whitelist = ['http://localhost:5000', 'http://localhost:5001', 'https://breaddit-885b4.firebaseapp.com', 'https://https://breaddit-885b4.web.app']
+var whitelist = ['http://localhost:5000', 'https://breaddit-885b4.firebaseapp.com', 'https://breaddit-885b4.web.app']
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
