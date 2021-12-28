26589e3db3efd0bebcf38111f7f7a705cb573d27
DPain <dpainhahn@gmail.com>
2021-12-28 01:45:46 -0500

    temp.

---
+++ breaddit-ui/src/services/recent.js
@@ -0,0 +1,11 @@
+const fetchRecent = function() {
+  return [
+    {
+      title: "Welcome!",
+      body:
+        "Click load to start analyzing your <b>auth.log</b> file!<br>" +
+        "Don't worry, your data all processed within your local machine and is not sent to us!",
+    },
+  ];
+};
+export { fetchRecent };
--- breaddit-ui/src/views/Index.vue
+++ breaddit-ui/src/views/Index.vue
@@ -22,93 +22,45 @@
         </div>
       </b-col>
     </b-row>
-    <b-row v-for="(item, i) in entries" :key="i">
-      <b-col>
-        <b-card class="mb-1 mt-1 p-3" raised no-body>
-          <b-card-title v-text="item.title"></b-card-title>
-          <b-card-text v-html="item.body"></b-card-text>
-        </b-card>
-      </b-col>
-    </b-row>
+    <div id="recent">
+      <b-row v-for="(item, i) in entries" :key="i">
+        <b-col>
+          <b-card class="mb-1 mt-1 p-3" raised no-body>
+            <b-card-title v-text="item.title"></b-card-title>
+            <b-card-text v-html="item.body"></b-card-text>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
   </div>
 </template>
 
 <script>
+import {fetchRecent} from '@/services/recent'
+
 export default {
+  name: "Index",
   props: {
     source: String
   },
 
-  data: () => ({
-    subreaddit: "",
-    entries: [
-      {
-        title: "Welcome!",
-        body:
-          "Click load to start analyzing your <b>auth.log</b> file!<br>" +
-          "Don't worry, your data all processed within your local machine and is not sent to us!"
-      },
-      {
-        title: "What is this?",
-        body:
-          "This web app takes in your <b>auth.log</b> file and returns useful information that can make you more informed about attacks towards your system.<br>" +
-          "Simply select your <b>auth.log</b> file and the application will handle the rest for you."
-      },
-      {
-        title: "What can I learn?",
-        body:
-          "The things you can learn from your <b>auth.log</b> file are the following:<br>" +
-          "<ul><li>Attacks per IP.</li>" +
-          "<li>Top Usernames used.</li>" +
-          "<li>Attack frequency over time.</li>" +
-          "<li>Attacks per city/country. (WIP)</li></ul>"
-      },
-      {
-        title: "What can I learn?",
-        body:
-          "The things you can learn from your <b>auth.log</b> file are the following:<br>" +
-          "<ul><li>Attacks per IP.</li>" +
-          "<li>Top Usernames used.</li>" +
-          "<li>Attack frequency over time.</li>" +
-          "<li>Attacks per city/country. (WIP)</li></ul>"
-      },
-      {
-        title: "Welcome!",
-        body:
-          "Click load to start analyzing your <b>auth.log</b> file!<br>" +
-          "Don't worry, your data all processed within your local machine and is not sent to us!"
-      },
-      {
-        title: "What is this?",
-        body:
-          "This web app takes in your <b>auth.log</b> file and returns useful information that can make you more informed about attacks towards your system.<br>" +
-          "Simply select your <b>auth.log</b> file and the application will handle the rest for you."
-      },
-      {
-        title: "What is this?",
-        body:
-          "This web app takes in your <b>auth.log</b> file and returns useful information that can make you more informed about attacks towards your system.<br>" +
-          "Simply select your <b>auth.log</b> file and the application will handle the rest for you."
-      },
-      {
-        title: "Welcome!",
-        body:
-          "Click load to start analyzing your <b>auth.log</b> file!<br>" +
-          "Don't worry, your data all processed within your local machine and is not sent to us!"
-      },
-      {
-        title: "What can I learn?",
-        body:
-          "The things you can learn from your <b>auth.log</b> file are the following:<br>" +
-          "<ul><li>Attacks per IP.</li>" +
-          "<li>Top Usernames used.</li>" +
-          "<li>Attack frequency over time.</li>" +
-          "<li>Attacks per city/country. (WIP)</li></ul>"
-      }
-    ],
-    drawer: false,
-    model: 1
-  })
+  data: function() {
+    return {
+      subreaddit: "",
+      entries: [
+        {
+          title: "Loading...",
+          body: "Loading..."
+        }
+      ],
+      drawer: false,
+      model: 1
+    }
+  },
+
+  created: async function() {
+    this.entries = await fetchRecent()
+  }
 };
 </script>
 
